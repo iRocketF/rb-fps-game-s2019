4f858175c0ac09650ccec4c8621238cb18eb34af
HautalaErno <36363438+HautalaErno@users.noreply.github.com>
2019-01-23 12:42:50 +0200

    MainMenu Foundation

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void PressPlay ()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void SettingsMenu()
+    {
+        // Activate Settings object and disable other buttons
+    }
+    public void QuitPutton ()
+    {
+        Application.Quit();
+    }
+
+}
