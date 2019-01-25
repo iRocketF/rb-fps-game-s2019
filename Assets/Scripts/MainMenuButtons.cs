2db9ae2e245623911892bfac7fd829b924de3a3c
eRno <ernohautala6@gmail.com>
2019-01-25 20:36:41 +0200

    MainMenu and settings foundation re-added

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+    public void ActiveSett() {
+        //Activate settings and disable other mainmenu buttons
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsScript : MonoBehaviour
+{
+    bool isPaused;
+    void Start()
+    {
+        isPaused = true;
+       //Default deactivate all children of this gameObject at the start of the game
+    }
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown("Escape") && !isPaused) {
+            Time.timeScale = 0;
+            //Activate all children under this gameobject
+            isPaused = true;
+        }
+        else if (Input.GetKeyDown("Escape") && isPaused) {   
+            UnPause();   
+        }
+    }
+    public void UnPause () {
+        Time.timeScale = 1;
+        //Deactivate all children under this gameobject
+        isPaused = false;
+    }
+}
