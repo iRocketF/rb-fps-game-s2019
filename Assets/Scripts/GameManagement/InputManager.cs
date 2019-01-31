784c4692aa568766f1dc543bee4ef8afee3a2662
HautalaErno <36363438+HautalaErno@users.noreply.github.com>
2019-01-31 14:59:49 +0200

    MainMenu Scaling

---
+++ Assets/Scripts/GameManagement/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputManager
+{
+    public static bool AButton()
+    {
+        return Input.GetButtonDown("A_Button");
+    }
+    public static bool AButton()
+    {
+        return Input.GetButtonDown("A_Button");
+    }
+    public static bool AButton()
+    {
+        return Input.GetButtonDown("A_Button");
+    }
+    public static bool AButton()
+    {
+        return Input.GetButtonDown("A_Button");
+    }
+    
+}
