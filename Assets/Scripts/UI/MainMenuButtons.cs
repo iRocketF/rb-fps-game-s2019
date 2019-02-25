cbb3528f5fdb2ba69560550622ee1a2b9e6f75cf
eRno <ernohautala6@gmail.com>
2019-02-25 15:01:37 +0200

    UI stuff

--- Assets/Scripts/UI/MainMenuButtons.cs
+++ Assets/Scripts/UI/MainMenuButtons.cs
@@ -6,14 +6,29 @@ using UnityEngine.UI;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    public GameObject settings;
+    public bool options;
+
+    void Start() {
+        settings.SetActive(false);
+    }
     public void Play() {
         SceneManager.LoadScene(1);
     }
     public void ActiveSett() {
-        //Activate settings and disable other mainmenu buttons
+        DisableChildren();
+        settings.SetActive(true);
     }
 
     public void QuitGame() {
         Application.Quit();
     }
+
+    public void DisableChildren()  
+{    
+    foreach (Transform child in transform)     
+    {  
+        child.gameObject.SetActive(false);   
+    }   
+}
 }
