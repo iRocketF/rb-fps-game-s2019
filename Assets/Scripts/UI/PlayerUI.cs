8e25384676c0080bf24c5bd0c2c34360069e3062
Erno <ernohautala6@gmail.com>
2019-03-04 13:08:15 +0200

    PlayerUI

--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerUI : MonoBehaviour
 {
     public Image hpBar;
     public Health hp;
+    public PlayerMovement pMov;
+    public TextMeshProUGUI blinks;
+    public Image blinkCD;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        blinkCD.fillAmount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        hpBar.fillAmount = hp.health / 100;
+        hpBar.fillAmount = hp.health / hp.maxhealth;
+        blinks.text = pMov.currentBlinks.ToString();
+        blinkCD.fillAmount = pMov.nextBlinkTimer / pMov.blinkCoolDown;
+
     }
 }
