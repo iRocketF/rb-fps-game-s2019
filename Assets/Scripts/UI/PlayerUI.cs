84eaa25926903d7cfc49f969635f6294f092e00b
Erno <ernohautala6@gmail.com>
2019-03-07 13:14:21 +0200

    AmmoUI CODED(needs art)

--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -11,11 +11,19 @@ public class PlayerUI : MonoBehaviour
     public PlayerMovement pMov;
     public TextMeshProUGUI blinks;
     public Image blinkCD;
+    public TextMeshProUGUI ammo;
+    public WeaponAmmo wpAmmo0;
+    public WeaponAmmo wpAmmo1;
+    public WeaponAmmo wpAmmo2;
+    public WeaponSwitch wpSwitch;
+    public int weaponSelected;
+
 
     // Start is called before the first frame update
     void Start()
     {
         blinkCD.fillAmount = 0;
+        
     }
 
     // Update is called once per frame
@@ -24,6 +32,28 @@ public class PlayerUI : MonoBehaviour
         hpBar.fillAmount = hp.health / hp.maxhealth;
         blinks.text = pMov.currentBlinks.ToString();
         blinkCD.fillAmount = pMov.nextBlinkTimer / pMov.blinkCoolDown;
+        WeaponGetter();
+        WpAmmoText();
+       // ammo.text = wpAmmo.currentAmmo + " / " + wpAmmo.maxAmmo;
+    }
+    void WeaponGetter()
+    {
+        weaponSelected = wpSwitch.selectedWeapon;
+    }
+    void WpAmmoText()
+    {
+        if (weaponSelected == 0)
+        {
+            ammo.text = wpAmmo0.currentAmmo + " / " + wpAmmo0.maxAmmo;
+        }
+        else if (weaponSelected == 1)
+        {
+            ammo.text = wpAmmo1.currentAmmo + " / " + wpAmmo1.maxAmmo;
+        }
+        else if (weaponSelected == 2)
+        {
+            ammo.text = wpAmmo2.currentAmmo + " / " + wpAmmo2.maxAmmo;
+        }
 
     }
 }
--- Assets/Scripts/Weapons/HitScanWeapon.cs
+++ Assets/Scripts/Weapons/HitScanWeapon.cs
@@ -26,57 +26,57 @@ public class HitScanWeapon : MonoBehaviour
     }
 
     void Update()
-<<<<<<< HEAD
+
     {
         if (Input.GetAxis(fireString) > 0.5f && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
-=======
-    {
 
-        if (ammo.currentAmmo > 0)
         {
-            if (Input.GetAxis(fireString) > 0.5f && Time.time >= nextTimeToFire)
+
+            if (ammo.currentAmmo > 0)
             {
-                nextTimeToFire = Time.time + 1f / fireRate;
-                ammo.currentAmmo--;
-                Shoot();
-            } 
-        }
-        else if (ammo.currentAmmo == 0)
-        {
-            if (!rtPressed && Input.GetAxis(fireString) > 0.5f && Time.time >= nextTimeToFire)
-            {
-                rtPressed = true;
-                AudioManager.instance.PlaySound("sound_ammoEmpty");
+                if (Input.GetAxis(fireString) > 0.5f && Time.time >= nextTimeToFire)
+                {
+                    nextTimeToFire = Time.time + 1f / fireRate;
+                    ammo.currentAmmo--;
+                    Shoot();
+                }
             }
-            if (Input.GetAxis(fireString) < 0.2)
+            else if (ammo.currentAmmo == 0)
             {
-                rtPressed = false;
+                if (!rtPressed && Input.GetAxis(fireString) > 0.5f && Time.time >= nextTimeToFire)
+                {
+                    rtPressed = true;
+                    AudioManager.instance.PlaySound("sound_ammoEmpty");
+                }
+                if (Input.GetAxis(fireString) < 0.2)
+                {
+                    rtPressed = false;
+                }
             }
-        }
 
->>>>>>> master
-    }
-
-    void Shoot()
-    {
-        AudioManager.instance.PlaySound("Sound_Shot");
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit))
-        {
-            hitObject = hit.transform.gameObject;
-            Health target = hitObject.GetComponent<Health>();
-
-            if (target != null)
-            {
-                target.TakeDamage(damage);
-            }
         }
     }
-}
+ 
+        void Shoot()
+        {
+            AudioManager.instance.PlaySound("Sound_Shot");
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit))
+            {
+                hitObject = hit.transform.gameObject;
+                Health target = hitObject.GetComponent<Health>();
+
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+            }
+        }
+    }
 
